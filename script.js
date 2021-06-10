03e1c33a1307c8f2e4166f9a05100549bb39d429
TrentHodgins <84517266+Trent-Hodgins@users.noreply.github.com>
2021-06-10 16:20:20 +0000

    Done fr this time

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // These define our variables.//
-let number = 15
+const number = 15
 let guess = 0
 
 // The "while" is for making a loop. This makes it so the user has to keep guessing untill they guess the right number.//
@@ -7,11 +7,11 @@ while (guess !== number) {
   guess = prompt('Guess my age')
   guess = parseInt(guess)
 
-// These are for telling the user if they need to guess higher or lower.//
+  // These are for telling the user if they need to guess higher or lower.//
   if (guess > number) {
     alert('Guess is too high.')
   } else if (guess < number) {
-   alert('Guess is too low.')
+    alert('Guess is too low.')
   } else if (guess === number) {
     alert('you got it right')
   }
